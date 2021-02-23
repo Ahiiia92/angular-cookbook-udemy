77266001064e79dd68cf8c24dcee2a6cf31ffe4d
Maroussia Arnault <maroussia.arnault@gmail.com>
2021-02-23 23:45:37 +0100

    homepage with an overwritten css property depending on the current url: done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
+import {Subscription} from "rxjs";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isHome = false;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router) {
+  }
+  ngOnInit() {
+    this.router.events.subscribe((e) => {
+      if (e instanceof NavigationEnd) {
+        e.url === '/' ? this.isHome = true : this.isHome = false;
+      }
+    });
+  }
 }
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.sass']
+  styleUrls: ['./homepage.component.sass'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomepageComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
