49b0de6d4a85e113d4f85d54c3ca95a217d03b23
Maroussia Arnault <maroussia.arnault@gmail.com>
2021-02-23 23:52:16 +0100

    refactoring of app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
-import {Subscription} from "rxjs";
 
 
 @Component({
